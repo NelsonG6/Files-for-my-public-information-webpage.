0d893b8801586326b44279db44fef17d9ae8b7c8
Nelson <18473267+NelsonRomaine@users.noreply.github.com>
2019-05-19 13:20:28 -0700

    update 3
    CSS and Jquery working now

---
+++ testing/javascript.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+    $("button").on("click", function() {
+       $(this).css("color", "#ff99ff");
+       $(this).css("background-color", "#000066");
+   });
+});
