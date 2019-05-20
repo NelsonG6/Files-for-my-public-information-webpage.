5e4e67ded5ef02ed580eab57fcc0f9a2165e96a5
Nelson <18473267+NelsonRomaine@users.noreply.github.com>
2019-05-19 20:04:08 -0700

    slideshow and vertical tabs
    Slideshow now working within vertical tabs

--- vtab.js
+++ vtab.js
@@ -1,20 +1,22 @@
-function openTab(evt, cityName) {
+function openvTab(evt, tabName) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
   // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
+  tabcontent = document.getElementsByClassName("vtabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
   // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
+  tablinks = document.getElementsByClassName("vtablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
   // Show the current tab, and add an "active" class to the link that opened the tab
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+document.getElementById("defaultOpen2").click();
