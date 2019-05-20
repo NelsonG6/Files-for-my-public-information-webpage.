38581590ee3766b414b7147beb1c1ab840c45a4c
Nelson <18473267+NelsonRomaine@users.noreply.github.com>
2019-05-20 00:21:16 -0700

    Deleting unnecessary things
    Removed a few folders that weren't needed

--- testing/javascript.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-    $("button").on("click", function() {
-       $(this).css("color", "#ff99ff");
-       $(this).css("background-color", "#000066");
-   });
-});
